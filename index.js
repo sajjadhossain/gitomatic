33a577400443ad80f9291fe3b77cb46cf6de0526
sajjadhossain <sajjad@withpulp.com>
2017-02-02 18:04:09 -0500

    Sun Jan 29 2017 18:04:05 GMT-0500 (EST)

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-var gitCommits = require('git-commits');
+var shell = require('shelljs');
+var beforeOneWeek = new Date(new Date().getTime() - 60 * 60 * 24 * 7 * 1000)
+  , day = beforeOneWeek.getDay()
+  , diffToMonday = beforeOneWeek.getDate() - day + (day === 0 ? -6 : 1)
+  , lastMonday = new Date(beforeOneWeek.setDate(diffToMonday))
+  , lastSunday = new Date(beforeOneWeek.setDate(diffToMonday + 6));
 
 module.exports = {
   getLastPushesSinceSunday: function() {
-    
+
+    // 2014-02-12T16:36:00-07:00
+    var run = shell.exec(
+      'git add -A . && ' +
+      'git commit -m "' + lastSunday + '" --GIT_AUTHOR_DATE="' + lastSunday + '" --GIT_COMMITTER_DATE= "' + lastSunday + '"',
+      {silent:true}
+    )
+    var statusCode = run.code;
+    var stderr = run.stderr;
+    var stdout = run.stdout;
+    var output = {
+      statusCode,
+      stderr,
+      stdout
+    };
+
+    return output;
   }
 };
