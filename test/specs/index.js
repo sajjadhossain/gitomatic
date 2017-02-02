ad813133cb3a4ef1371fdd2f35d907e2939fa0c5
sajjadhossain <sajjad@withpulp.com>
2017-02-02 17:20:44 -0500

    rm node modules

--- test/specs/index.js
+++ test/specs/index.js
@@ -1,7 +1,8 @@
 describe('git account features', function() {
   it('gets log since last Sunday', function() {
     var gitomatic = require('../../index');
+    var getLastPushesSinceSunday = gitomatic.getLastPushesSinceSunday();
 
-    console.log(gitomatic.getLastPushesSinceSunday());
+    console.log(getLastPushesSinceSunday);
   })
 })
